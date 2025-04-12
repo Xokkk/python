e3506a3e3735b4a60001736d3bf9de79839db8b6
Xokkk <kyleysw@gmail.com>
2025-04-13 02:19:13 +0900

    Add files via upload

---
+++ 250411/formatting.py
@@ -0,0 +1,19 @@
+a = 'dont feel sad'
+print(a)
+print(a.count('d'))
+print(a.find('k'))
+print(a.index('f'))
+print(",".join('dont'))
+print(a.upper())
+print(a.lower())
+print(a.lstrip())
+print(a.rstrip())
+
+b = 'HelloWorld!'
+print(b)
+
+b = input()
+print(b)
+
+a, b = map(int, input().strip().split(' '))
+
